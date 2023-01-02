4c2d58d3dae456210bfaadde92e0660315004178
chandrakala <chandhu6302886523@gmail.com>
2023-01-02 19:18:21 +0530

    java2

---
+++ java2.js
@@ -0,0 +1,9 @@
+//If a person is allowed to drive in India print "Apply for a license" or "NA".
+//let name=pavani;
+let age=19;
+license=18;
+if(18<19){
+  console.log("Apply for license");
+}else{
+  console.log("NA");
+}
