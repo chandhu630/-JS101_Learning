69fcbd3e6cf5b34f4ec39f160e89933b469e2fcf
chandrakala <chandhu6302886523@gmail.com>
2023-01-02 19:04:05 +0530

    java1

---
+++ java1.js
@@ -0,0 +1,5 @@
+// Print "Masai School" in the console followed by "A Transformation in Education" in next line
+let name="Masai School";
+console.log(name);
+let name1="A Transformation in Education";
+console.log(name1);
